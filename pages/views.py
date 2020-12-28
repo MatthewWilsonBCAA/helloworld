a74340699cfe92027e3c59b17d893a45ad15d9b8
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-12-28 10:42:39 -0600

    made first dynamic "website"

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+import random
+from django.http import HttpResponse
+
+
+def homePageView(request):
+    return HttpResponse(random.randint(0, 100))

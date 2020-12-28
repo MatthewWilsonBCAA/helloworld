7f7e840bc05ec453a553bca44286deb2eba36865
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-12-28 11:14:02 -0600

    added an about page, and actual templates

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,12 @@
 
 # Create your views here.
 import random
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def homePageView(request):
-    return HttpResponse(random.randint(0, 100))
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
